8c115e9f5aa36894cb7c511fa60f33069c1837a8
Juan R <juangnba@hotmail.com>
2023-02-22 17:55:27 +0100

    added getscore util

---
+++ utils/getScore.js
@@ -0,0 +1,23 @@
+const Restroom = require("../models/Restroom.model")
+
+const getScore = restroomID => {
+    return Restroom
+        .findById(restroomID)
+        .populate("votes")
+        .select({ votes: 1 })
+        .then(({ votes }) => {
+            console.log(votes)
+
+            const votesCount = votes.reduce((acc, elm) => {
+                const { vote } = elm
+                vote === 'up' ? acc++ : acc--
+
+                return acc
+            }, 0)
+            console.log(votesCount)
+            return votesCount ? (votesCount / votes.length) * 100 : 0
+        })
+        .catch(err => next(err))
+}
+
+module.exports = { getScore }
